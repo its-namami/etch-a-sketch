532b4cde8e425f08e1b9a82e55995e14826d1c83
its-namami <164639260+its-namami@users.noreply.github.com>
2025-06-20 17:58:37 +0200

    feat: add adjustability to grid size

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,10 @@
-const mainElement = document.querySelector('main#flexbox-16x16');
+// TO-DO: Add Dialog
+// Add possibility to change each grid height
+// Add Preview to the Dialog
+const wholeGrid = document.querySelector('div#flexbox-16x16');
+const newGridBtn = document.querySelector('button#invoke-choosing-new-grid');
+const newGridDialog = document.querySelector('dialog#new-grid-dialog');
+const closeNewGridDialog = document.querySelector('dialog#new-grid-dialog .close-dialog');
 
 const itemFlexbox = function createAndAppendFlexboxItem(appendToElement) {
   const itemFlexbox = document.createElement('div');
@@ -14,25 +20,72 @@ const flexboxRow = function createAndAppendHorizontalFlexbox(appendToElement) {
   return horizontalFlexbox;
 }
 
-const createGrid = function create16x16GridOfPredefinedVariables() {
-  for (let i = 0; i < 4; i++) { 
-    const flexboxRowElement = flexboxRow(mainElement);
-    for (let i = 0; i < 4; i++) {
+const createGrid = function createGridOfPredefinedVariables(rows, colons) {
+  const flexboxArray = [];
+  for (let i = 0; i < rows; i++) { 
+    const flexboxRowElement = flexboxRow(wholeGrid);
+    for (let i = 0; i < colons; i++) {
       itemFlexbox(flexboxRowElement);
     }
+    flexboxArray.push(flexboxRowElement)
   }
+
+  const itemFlexboxes = Array.from(document.querySelectorAll('div#flexbox-16x16 .horizontal-flexbox .item-flexbox'));
+
+  itemFlexboxes.forEach(itemFlexbox => {
+    itemFlexbox.addEventListener('mousedown', () => {
+      itemFlexbox.classList.add('background-darkblue');
+    });
+
+    itemFlexbox.addEventListener('mousemove', () => {
+      if (mouseDown) {
+	itemFlexbox.classList.add('background-darkblue');
+      }
+    });
+  });
+
+  return flexboxArray;
 }
 
-createGrid();
+let thisGrid = createGrid(16, 32);
 
-const itemFlexboxes = Array.from(document.querySelectorAll('main#flexbox-16x16 .horizontal-flexbox .item-flexbox'));
 
-itemFlexboxes.forEach(itemFlexbox => {
-  itemFlexbox.addEventListener('mouseover', () => {
-    itemFlexbox.classList.add('background-darkblue');
-    setTimeout(() => {
-      itemFlexbox.classList.remove('background-darkblue');
-    }, 1000);
+let mouseDown = false;
+let drawMode = true;
+let eraseMode = false;
+
+document.addEventListener('mousedown', () => {
+  mouseDown = true;
+});
+
+document.addEventListener('mouseup', () => {
+  mouseDown = false;
+});
+newGridBtn.addEventListener('click', () => {
+  newGridDialog.showModal();
+});
+
+closeNewGridDialog.addEventListener('mouseenter', () => {
+  closeNewGridDialog.querySelector('span').textContent = '󰅙';
+});
+
+closeNewGridDialog.addEventListener('mouseleave', () => {
+  closeNewGridDialog.querySelector('span').textContent = '󰻂';
+});
+
+closeNewGridDialog.addEventListener('click', () => {
+  newGridDialog.close();
+});
+
+newGridDialog.addEventListener('submit', event => {
+  event.preventDefault();
+  const rows = newGridDialog.querySelector('form').elements['rows'].value;
+  const columns = newGridDialog.querySelector('form').elements['columns'].value;
+  thisGrid.forEach(row => {
+    row.remove();
   });
+  thisGrid = createGrid(rows, columns);
+  newGridDialog.close();
+
 });
 
