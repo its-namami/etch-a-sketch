b2bbf23d583a35518ef2b8bc4ca615e37a962cca
its-namami <164639260+its-namami@users.noreply.github.com>
2025-06-28 23:19:32 +0200

    feat: add and style color picker, add scaling size

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,5 @@
 // TO-DO: Add Dialog
-// Add possibility to change each grid height
-// Add Preview to the Dialog
+// Add Download Function
 const wholeGrid = document.querySelector('section#sketch-grid');
 const newGridBtn = document.querySelector('button#invoke-choosing-new-grid');
 const newGridDialog = document.querySelector('dialog#new-grid-dialog');
@@ -8,12 +7,25 @@ const closeNewGridDialog = document.querySelector('dialog#new-grid-dialog #close
 const minimizeNewGridDialog = document.querySelector('dialog#new-grid-dialog #minimize-new-grid-dialog-button');
 const toolbar = document.querySelector('#toolbar');
 const toolbarDrag = document.querySelector('#toolbar-drag');
+const colorPickerBtn = document.querySelector('#color-picker');
 const drawModeBtn = document.querySelector('#draw-mode');
 const eraseModeBtn = document.querySelector('#erase-mode');
 const newGridDialogDrag = document.querySelector('dialog#new-grid-dialog .controls-dialog');
+const colorPickerDiv = document.querySelector('div#color-picker-div');
+const colorPickerPopupContainer = document.querySelector('div#color-picker-picker');
+const colorPickerPopupDrag = colorPickerPopupContainer.querySelector('.controls-dialog');
+const closeColorPicker = colorPickerPopupDrag.querySelector('#close-color-picker-button');
+const minimizeColorPicker = colorPickerPopupDrag.querySelector('#minimize-color-picker-button');
+const colorPickerSubmitBtn = colorPickerPopupContainer.querySelector('button[type="submit"]');
+const colorPickerRow = document.querySelector('#color-picker-row');
+const colorPicker = document.querySelector('#color-picker-row input[type="color"]');
+const colorPickerHex = document.querySelector('#color-picker-hex');
+const previousColorsContainer = colorPickerPopupContainer.querySelector('#previous-colors-flexbox');
+const previousColorsArray = [];
+const gridSizeInput = document.querySelector('#grid-size-input');
 
 let mouseDown = false;
-let drawMode = false;
+let drawMode = true;
 let eraseMode = false;
 let isToolbarDragging = false
 let offsetX;
@@ -21,13 +33,48 @@ let newGridDialogMouseDown = false;
 let newGridDialogOffsetX;
 let newGridDialogOffsetY;
 let firstClickedOnControlButton = false;
+let colorPicked = '#000000'
+let colorPickerDragging;
+let colorPickerOffsetX;
+let colorPickerOffsetY;
+
+colorPickerPopupDrag.addEventListener('mousedown', e => {
+  colorPickerDragging = true;
+  colorPickerOffsetY = e.offsetY;
+  colorPickerOffsetX = e.offsetX;
+});
+
+const getHex = function getHexColorFromString(str){
+      const ctx = document.createElement('canvas').getContext('2d');
+      ctx.fillStyle = str;
+      return ctx.fillStyle;
+}
+
+colorPickerPopupContainer.addEventListener('click', e => {
+  thisBackgroundColor = e.target.style.backgroundColor;
+  if (e.target.classList.contains('previous-color')) {
+    colorPicked = getHex(thisBackgroundColor);
+    colorPicker.value = getHex(thisBackgroundColor);
+    colorPickerHex.textContent = getHex(thisBackgroundColor);
+  }
+});
+
+colorPickerPopupContainer.addEventListener('mousedown', () => {
+  colorPickerPopupContainer.classList.add('focused');
+  toolbar.classList.remove('focused');
+});
+
+colorPicker.addEventListener('change', e => {
+  colorPicked = getHex(colorPicker.value);
+  console.log(colorPicked)
+});
+
+colorPicker.addEventListener('input', e => {
+  colorPickerHex.textContent = getHex(colorPicker.value);
+  colorPicked = getHex(colorPicker.value);
+});
 
 newGridDialogDrag.addEventListener('mousedown', e => {
-  console.dir(e);
-    console.log(`Screen Y: ${e.screenY}`);
-    console.log(`newGridDialogOffsetY = ${e.clientY} - ${newGridDialog.getBoundingClientRect().top}`);
-    console.log(` offsetX = (${e.offsetY} - ${e.y})`);
-
   if (e.target !== closeNewGridDialog && e.target !== minimizeNewGridDialog) {
     newGridDialogMouseDown = true;
     newGridDialogOffsetX = e.clientX - newGridDialog.getBoundingClientRect().left;
@@ -36,21 +83,22 @@ newGridDialogDrag.addEventListener('mousedown', e => {
 });
 
 drawModeBtn.addEventListener('click', () => {
-  drawMode = true;
+  drawMode == true ? drawMode = false : drawMode = true;
   eraseMode = false;
-  drawModeBtn.classList.add('draw-active');
+  drawModeBtn.classList.toggle('draw-active');
   eraseModeBtn.classList.remove('erase-active');
 });
 
 eraseModeBtn.addEventListener('click', () => {
   drawMode = false;
-  eraseMode = true;
+  eraseMode == true ? eraseMode = false : eraseMode = true;
   drawModeBtn.classList.remove('draw-active');
   eraseModeBtn.classList.toggle('erase-active');
 });
 
 toolbarDrag.addEventListener('mousedown', e => {
   isToolbarDragging = true;
+  offsetY = e.offsetY;
 });
 
 document.addEventListener('mousedown', e => {
@@ -62,7 +110,8 @@ document.addEventListener('mousedown', e => {
 document.addEventListener('mousemove', e => {
   if (isToolbarDragging) {
     toolbar.style.left = `${e.screenX - offsetX}px`;
-    toolbar.style.top = `${e.screenY}px`;
+    toolbar.style.top = `${e.y - offsetY - toolbarDrag.offsetTop}px`;
+    toolbar.style.transform = 'none';
   }
 
   if (newGridDialogMouseDown && !firstClickedOnControlButton) {
@@ -70,10 +119,17 @@ document.addEventListener('mousemove', e => {
     newGridDialog.style.top = `${e.y - newGridDialogOffsetY}px`;
     newGridDialog.style.margin = 0;
   }
+
+  if (colorPickerDragging && !firstClickedOnControlButton) {
+    if (e.y - colorPickerOffsetY + 'px') colorPickerPopupContainer.style.top = e.y - colorPickerOffsetY + 'px';
+    if (e.x - colorPickerOffsetX + 'px') colorPickerPopupContainer.style.left = e.x - colorPickerOffsetX + 'px';
+  }
 });
 
 wholeGrid.addEventListener('mousedown', () => {
   mouseDown = true;
+  createPrevColor(colorPicker.value);
+  colorPickerBtn.style.backgroundColor = getHex(colorPicker.value) + '50';
 });
 
 document.addEventListener('mouseup', () => {
@@ -81,6 +137,7 @@ document.addEventListener('mouseup', () => {
   newGridDialogMouseDown = false;
   mouseDown = false;
   isToolbarDragging = false;
+  colorPickerDragging = false;
 });
 
 newGridBtn.addEventListener('click', () => {
@@ -101,11 +158,11 @@ const flexboxRow = function createAndAppendHorizontalFlexbox(appendToElement) {
   return horizontalFlexbox;
 }
 
-const createGrid = function createGridOfPredefinedVariables(rows, colons) {
+const createGrid = function createGridOfPredefinedVariables(rows, columns) {
   const flexboxArray = [];
   for (let i = 0; i < rows; i++) { 
     const flexboxRowElement = flexboxRow(wholeGrid);
-    for (let i = 0; i < colons; i++) {
+    for (let i = 0; i < columns; i++) {
       itemFlexbox(flexboxRowElement);
     }
     flexboxArray.push(flexboxRowElement)
@@ -115,14 +172,14 @@ const createGrid = function createGridOfPredefinedVariables(rows, colons) {
 
   itemFlexboxes.forEach(itemFlexbox => {
     itemFlexbox.addEventListener('mousedown', () => {
-      if (drawMode) itemFlexbox.classList.add('background-darkblue');
-      if (eraseMode) itemFlexbox.classList.remove('background-darkblue');
+      if (drawMode) itemFlexbox.style.backgroundColor = colorPicked;
+      if (eraseMode) itemFlexbox.style.backgroundColor = '';
     });
 
     itemFlexbox.addEventListener('mousemove', () => {
       if (mouseDown) {
-	if (drawMode) itemFlexbox.classList.add('background-darkblue');
-	if (eraseMode) itemFlexbox.classList.remove('background-darkblue');
+	if (drawMode) itemFlexbox.style.backgroundColor = colorPicked;
+	if (eraseMode) itemFlexbox.style.backgroundColor = '';
       }
     });
   });
@@ -130,7 +187,25 @@ const createGrid = function createGridOfPredefinedVariables(rows, colons) {
   return flexboxArray;
 }
 
+// Dark-Color-Checker
+function hex_is_dark(color) {
+      const hex = color.replace('#', '');
+      const c_r = parseInt(hex.substring(0, 0 + 2), 16);
+      const c_g = parseInt(hex.substring(2, 2 + 2), 16);
+      const c_b = parseInt(hex.substring(4, 4 + 2), 16);
+      const brightness = ((c_r * 299) + (c_g * 587) + (c_b * 114)) / 1000;
+      return brightness < 128;
+}
+
 let thisGrid = createGrid(16, 16);
+
+minimizeColorPicker.addEventListener('mousedown', () => {
+  firstClickedOnControlButton = true;
+});
+
+closeColorPicker.addEventListener('mousedown', () => {
+  firstClickedOnControlButton = true;
+});
 
 minimizeNewGridDialog.addEventListener('mousedown', () => {
   firstClickedOnControlButton = true;
@@ -138,6 +213,14 @@ minimizeNewGridDialog.addEventListener('mousedown', () => {
 
 closeNewGridDialog.addEventListener('mousedown', () => {
   firstClickedOnControlButton = true;
+});
+
+closeColorPicker.addEventListener('mouseenter', () => {
+  closeColorPicker.querySelector('span').textContent = '󰅙';
+});
+
+closeColorPicker.addEventListener('mouseleave', () => {
+  closeColorPicker.querySelector('span').textContent = '󰻂';
 });
 
 closeNewGridDialog.addEventListener('mouseenter', () => {
@@ -153,12 +236,28 @@ closeNewGridDialog.addEventListener('click', () => {
   newGridDialog.close();
 });
 
+minimizeColorPicker.addEventListener('mouseenter', () => {
+  minimizeColorPicker.querySelector('span').textContent = '󰅙';
+});
+
+minimizeColorPicker.addEventListener('mouseleave', () => {
+  minimizeColorPicker.querySelector('span').textContent = '';
+});
+
 minimizeNewGridDialog.addEventListener('mouseenter', () => {
   minimizeNewGridDialog.querySelector('span').textContent = '';
 });
 
 minimizeNewGridDialog.addEventListener('mouseleave', () => {
   minimizeNewGridDialog.querySelector('span').textContent = '';
+});
+
+closeColorPicker.addEventListener('click', () => {
+  colorPickerPopupContainer.classList.add('invisible');
+});
+
+minimizeColorPicker.addEventListener('click', () => {
+  colorPickerPopupContainer.classList.add('invisible');
 });
 
 minimizeNewGridDialog.addEventListener('click', () => {
@@ -169,18 +268,25 @@ newGridDialog.addEventListener('keydown', event => {
   if (event.key === 'Escape') newGridMouseDown = false;
 });
 
+const createPrevColor = function createPreviousColorIfExistsRemovePrevious(color) {
+  let isUnique;
+  const previousColor = document.createElement('div');
+  previousColor.classList.add('previous-color');
+  if (hex_is_dark(color)) previousColor.classList.add('previous-color-dark');
+  previousColor.style.backgroundColor = color;
+  previousColorsArray.forEach(element => {
+    if (previousColor.style.backgroundColor === element.style.backgroundColor) {
+      element.remove();
+    }
+  });
+  previousColorsContainer.prepend(previousColor);
+  previousColorsArray.push(previousColor);
+}
+
 newGridDialog.addEventListener('submit', event => {
   event.preventDefault();
   const rows = newGridDialog.querySelector('form').elements['rows'].value;
   const columns = newGridDialog.querySelector('form').elements['columns'].value;
-  const gridItemSizeValue = newGridDialog.querySelector('form').elements['grid-item-size-value'].value;
-  const gridItemSizeUnit = newGridDialog.querySelector('form').elements['grid-item-size-unit'].value;
-
-  if (gridItemSizeValue != '') {
-    wholeGrid.style.setProperty('--grid-item-size', `${gridItemSizeValue}${gridItemSizeUnit}`);
-  } else {
-    wholeGrid.style.setProperty('--grid-item-size', '0.9rem');
-  }
 
   thisGrid.forEach(row => {
     row.remove();
@@ -195,4 +301,19 @@ newGridDialog.addEventListener('submit', event => {
 
   newGridDialog.querySelector('form').reset();
   newGridDialog.close();
+});
+
+gridSizeInput.addEventListener('input', () => {
+    wholeGrid.style.setProperty('--grid-item-size', `${gridSizeInput.value}px`);
+});
+
+colorPickerBtn.addEventListener('click', () => {
+  colorPickerPopupContainer.classList.toggle('invisible');
+  toolbar.classList.remove('focused');
+  colorPickerPopupContainer.classList.add('focused');
+});
+
+toolbar.addEventListener('mousedown', () => {
+  colorPickerPopupContainer.classList.remove('focused');
+  toolbar.classList.add('focused');
 });
